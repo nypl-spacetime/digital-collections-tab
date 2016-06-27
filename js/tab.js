693afb87afcbac56269a7d4995c4d8a45c2827bc
Bert Spaan <bert.spaan@gmail.com>
2016-06-27 16:47:57 -0400

    nypl lion, header, title, promises!

--- js/tab.js
+++ js/tab.js
@@ -5,7 +5,7 @@ var appUrl = 'http://localhost:3000/#/'
 // TODO: add 'click here to geotag/open in surveyor button'
 // TODO: load MODS, add title + metadata
 
-function checkStatus (response) {
+const checkStatus = (response) => {
   if (response.status >= 200 && response.status < 300) {
     return response
   } else {
@@ -15,11 +15,15 @@ function checkStatus (response) {
   }
 }
 
-function parseJSON (response) {
-  return response.json()
-}
+const parseJSON = (response) => response.json()
 
-function loadItem (callback) {
+const callAPI = (path) => fetch(`${apiUrl}${path}`, {
+    credentials: 'include'
+  })
+  .then(checkStatus)
+  .then(parseJSON)
+
+const loadItem = (callback) => {
   fetch(`${apiUrl}items/random`, {
     credentials: 'include'
   })
@@ -32,13 +36,34 @@ function loadItem (callback) {
   })
 }
 
-loadItem((err, item) => {
-  if (err) {
-    console.error(err.message)
-    return
-  }
-
+const setImage = (item) => {
   var image = document.getElementById('image')
   image.style.backgroundImage = `url(${item.image_link})`
   image.href = `${appUrl}${item.uuid}`
-})
+
+  return item
+}
+
+const setHeader = (item, collections, mods) => {
+  var titleElement = document.getElementById('header-title')
+  var collectionElement = document.getElementById('header-collection')
+  var linkElement = document.getElementById('header-link')
+
+  titleElement.innerHTML = item.title
+
+  var collection = collections.filter((collection) => collection.uuid === item.collection)[0]
+  collectionElement.href = `http://digitalcollections.nypl.org/items/${item.collection}`
+  collectionElement.innerHTML = collection ? collection.title : ''
+
+  linkElement.href = `http://digitalcollections.nypl.org/items/${item.uuid}`
+}
+
+callAPI('items/random')
+  .then(setImage)
+  .then((item) => Promise.all([
+      item,
+      callAPI('collections'),
+      // callAPI(`items/${item.uuid}/mods`)
+  ]))
+  .then((results) => setHeader.apply(null, results))
+  .catch((err) => console.error(err.message))
