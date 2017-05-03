1052f1e972bb6b0396be135f5fe8b0a5bcb54262
Bert Spaan <bert.spaan@gmail.com>
2017-05-03 08:20:11 -0400

    new design, new API, added info dialog

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# NYPL Chrome New Tab Extension
+# NYPL Digital Collections Chrome New Tab Extension
 
-A beautiful item from The New York Public Library's [Digital Collections](http://digitalcollections.nypl.org/), every time you open a new tab!
+A beautiful photo from The New York Public Library's [Digital Collections](http://digitalcollections.nypl.org/), every time you open a new tab!
 
-# Installation
+![](images/screenshots/flatbush-avenue.jpg)
 
-From Chrome Web Store (TODO), or clone this repository, go to [chrome://extensions](chrome://extensions), enable _Developer mode_ and click [Load unpacked extension…](https://developer.chrome.com/extensions/getstarted#unpacked).
+If you know the location where the photo was taken, clicking the photo will take you to [Surveyor](http://spacetime.nypl.org/surveyor) where you can help geotag the photo collection of the New York Public Library.
+
+## Installation
+
+From Chrome Web Store (__coming soon!__), or clone this repository, go to [chrome://extensions](chrome://extensions), enable _Developer mode_ and click [Load unpacked extension…](https://developer.chrome.com/extensions/getstarted#unpacked).
--- js/tab.js
+++ js/tab.js
@@ -1,10 +1,14 @@
-var API_URL = 'http://brick-by-brick.dev/'
-var APP_URL = 'http://localhost:3224/#/'
+// Development:
+// var API_URL = 'http://brick-by-brick.dev/'
+// var APP_URL = 'http://localhost:3224/#/'
+
+// Production:
+var API_URL = 'http://brick-by-brick.herokuapp.com/'
+var APP_URL = 'http://spacetime.nypl.org/surveyor/#/'
+
 var ORGANIZATION = 'nypl'
+var MAX_TITLE_LENGTH = 150
 
-// var APP_URL = 'http://spacetime.nypl.org/surveyor/#/'
-
-// TODO: add 'click here to geotag/open in surveyor button'
 
 function checkStatus (response) {
   if (response.status >= 200 && response.status < 300) {
@@ -28,45 +32,160 @@ function callAPI (path) {
   .then(parseJSON)
 }
 
+function setMessage (message) {
+  var element = document.getElementById('message')
+  element.innerHTML = message
+}
+
+function showError (err) {
+  console.error(err)
+  setMessage('Failed to load image')
+}
+
+function imageLoaded () {
+  setMessage('')
+  document.getElementsByTagName('header')[0].classList.remove('hidden')
+  document.getElementsByTagName('footer')[0].classList.remove('hidden')
+  document.getElementById('image').classList.add('fade-in')
+}
+
+function showModal () {
+  document.getElementById('modal').classList.remove('hidden')
+  setTimeout(function (){
+    document.getElementById('modal').classList.add('fade-in')
+  }, 10)
+
+  document.querySelector('#modal article').scrollTop = 0
+}
+
+function hideModal () {
+  document.getElementById('modal').classList.remove('fade-in')
+
+  setTimeout(function (){
+    document.getElementById('modal').classList.add('hidden')
+  }, 250)
+}
+
+window.addEventListener('keydown', function (event) {
+  if (event.keyCode == 27) {
+    hideModal()
+  }
+})
+
+document.getElementById('back-button').addEventListener('click', hideModal)
+document.getElementById('modal').addEventListener('click', function (event) {
+  if (event.target === this) {
+    hideModal()
+  }
+})
+
 function setImage (item) {
   var image = document.getElementById('image')
-  var imageLink = document.getElementById('image-link')
+  var modalImage = document.getElementById('modal-image')
 
+  var title = item.data.title
   var imageUrls = item.data.image_urls
 
   // TODO: imageUrls[0] is always 760px wide,
   //   but if bigger sizes are available, they sometimes
   //   are rotated and include ruler and color strip
-  image.src = imageUrls[0].url
-  image.className = ''
-  image.alt = item.data.title
+  var imageUrl = imageUrls[0].url
 
-  imageLink.href = APP_URL + item.id
+  image.src = imageUrl
+  modalImage.src = imageUrl
+  image.classList.remove('hidden')
+
+  image.alt = item.data.title
+  modalImage.alt = title
+
+  if (image.complete) {
+    imageLoaded()
+  } else {
+    image.addEventListener('load', imageLoaded)
+    image.addEventListener('error', showError)
+  }
 
   return item
 }
 
-function setHeader (item) {
+function setMetadata (item) {
   var headerTitle = document.getElementById('header-title')
-  var headerCollection = document.getElementById('header-collection')
   var headerLink = document.getElementById('header-link')
-  var footerLink = document.getElementById('footer-link')
+  var surveyorLink = document.getElementById('surveyor-link')
+  var moreInformationLink = document.getElementById('more-information')
 
-  headerTitle.innerHTML = item.data.title
-  headerCollection.href = 'http://digitalcollections.nypl.org/items/' + item.collection.id
-  headerCollection.innerHTML = item.collection.title
+  var modalTitle = document.getElementById('modal-title')
+  var modalCollection = document.getElementById('modal-collection')
+  var modalLocation = document.getElementById('modal-location')
+  var modalDate = document.getElementById('modal-date')
+  var modalDigitalCollectionsLink = document.getElementById('modal-digital-collections-link')
+  var modalSurveyorLink = document.getElementById('modal-surveyor-link')
 
-  headerLink.href = 'http://digitalcollections.nypl.org/items/' + item.id
-  footerLink.href = APP_URL + item.id
+  moreInformationLink.addEventListener('click', showModal)
 
-  document.getElementsByTagName('header')[0].className = ''
-  document.getElementsByTagName('footer')[0].className = ''
+  // Break long titles on first space before MAX_TITLE_LENGTH
+  var title = item.data.title
+  if (title.length > MAX_TITLE_LENGTH) {
+    for (var i = MAX_TITLE_LENGTH; i > 0; i--) {
+      if (title[i] === ' ') {
+        title = title.slice(0, i) + ' …'
+        break
+      }
+    }
+  }
+
+  var digitalCollectionsHref = 'http://digitalcollections.nypl.org/items/' + item.id
+  var collectionHref = 'http://digitalcollections.nypl.org/items/' + item.collection.id
+  var surveyorHref = APP_URL + item.id
+
+  headerTitle.innerHTML = title
+  headerLink.href = digitalCollectionsHref
+  surveyorLink.href = surveyorHref
+
+  modalTitle.innerHTML = item.data.title
+  modalCollection.href = collectionHref
+  modalCollection.innerHTML = item.collection.title
+
+  if (item.data.date) {
+    modalDate.innerHTML = item.data.date
+    modalDate.parentElement.classList.remove('hidden')
+  } else {
+    modalDate.innerHTML = ''
+    modalDate.parentElement.classList.add('hidden')
+  }
+
+  if (item.data.location) {
+    modalLocation.innerHTML = item.data.location
+    modalLocation.parentElement.classList.remove('hidden')
+  } else {
+    modalLocation.innerHTML = ''
+    modalLocation.parentElement.classList.add('hidden')
+  }
+
+  modalDigitalCollectionsLink.href = digitalCollectionsHref
+  modalSurveyorLink.href = surveyorHref
 }
 
-callAPI('tasks/geotag-photo/items/random?organization=' + ORGANIZATION)
-  .then(setImage)
-  .then(setHeader)
-  .catch((err) => {
-    var message = document.getElementById('message')
-    message.innerHTML = 'Failed to load image'
-  })
+function setLoading () {
+  setMessage('Loading…')
+
+  document.getElementsByTagName('header')[0].classList.add('hidden')
+  document.getElementsByTagName('footer')[0].classList.add('hidden')
+  document.getElementById('image').classList.remove('fade-in')
+}
+
+function getItem (uuid) {
+  setLoading()
+
+  var url = 'tasks/geotag-photo/items/random?organization=' + ORGANIZATION
+  if (uuid) {
+    url = 'organizations/' + ORGANIZATION + '/items/' + uuid
+  }
+
+  callAPI(url)
+    .then(setImage)
+    .then(setMetadata)
+    .catch(showError)
+}
+
+getItem()
